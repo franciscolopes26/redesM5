94eeffbf5bd71a26d4ae5c39a58e7e80cd262b4d
francisco lopes <franciscolopesr26@gmail.com>
2020-10-09 18:25:30 +0100

    aula 09/10/20

--- Ficha3/js/script.js
+++ Ficha3/js/script.js
@@ -28,7 +28,53 @@ function changePic2() {
   document.getElementById("myImg").src = "smile1.png";
 }
 function toNegrito() {
-  var pegar = (document.getElementById("text").style.fontWeight = "bold");
-  text.style.font - weight - bold;
-  text.style.text - center;
+  let x = document.getElementsByClassName("negrito");
+
+  for (let i = 0; i < x.length; i++) {
+    x[i].classList.toggle("normal");
+  }
+}
+function listaa() {
+  let list = document.getElementById("listaIng");
+  let item = document.getElementById("item").value;
+  let imagem = document.createElement("img");
+  imagem.src = "lixo.png";
+  var entry = document.createElement("li");
+  entry.innerHTML = item;
+
+  entry.appendChild(imagem);
+  list.appendChild(entry);
+
+  imagem.onclick = function () {
+    list.removeChild(entry);
+  };
+  //list.appendChild(document.createTextNode(entry));
+  //imga.appendChild(document.createTextNode(imga));
+}
+function apagar() {
+  alert("funcao 2");
+  var image_x = document.getElementById("img");
+  let list = document.getElementById("listaIng");
+
+  //list.parentElement.remove();
+  list.parentNode.parentNode.parentNode.removeChild(list.parentNode.parentNode);
+  //list.parentNode.removeChild(list.parentNode);
+  // list.parentNode.removeChild(list).tabIndex(this);
+}
+function pegar() {
+  let checkedValue = "";
+  let nome = document.getElementById("inputNome");
+  let morada = document.getElementById("inputMorada");
+  let genero = document.getElementById("gridRadios");
+  let pais = document.getElementById("selectPais");
+  let hbies = document.getElementById("hobbies");
+  let resultado = document.getElementById("mensagem");
+  //let valuemarca = genero.options[genero.selectedIndex].text;
+  for (var i = 0; i <= 2; ++i) {
+    if (genero[i].checked) {
+      checkedValue = genero.text;
+    }
+  }
+  resultado.innerHTML =
+    nome.value + "<br/>" + morada.value + checkedValue.value;
 }
